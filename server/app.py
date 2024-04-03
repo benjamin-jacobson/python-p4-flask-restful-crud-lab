e8dc1b60505841132f8df619659b270bcbe52f24
BenjaminJacobson <ben.jacobson@flatironschool.com>
2024-04-02 23:46:42 -0400

    completed lab

--- server/app.py
+++ server/app.py
@@ -47,6 +47,21 @@ class PlantByID(Resource):
         plant = Plant.query.filter_by(id=id).first().to_dict()
         return make_response(jsonify(plant), 200)
 
+    def patch(self,id):
+        record = Plant.query.filter_by(id=id).first() #.to_dict()
+        data = request.get_json()
+        for attr in data:
+            setattr(record, attr, data[attr])
+        db.session.add(record)
+        db.session.commit()
+        return make_response(record.to_dict(),200)
+
+    def delete(self,id):
+        record = Plant.query.filter_by(id=id).first() #.to_dict()
+        db.session.delete(record)
+        db.session.commit()
+        response_dict = {"message":"record successfully deleted"}
+        return make_response(response_dict,204)
 
 api.add_resource(PlantByID, '/plants/<int:id>')
 
